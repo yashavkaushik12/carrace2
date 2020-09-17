bd58ea81ef8845c884933c3e213fdc7819cd9766
yashavkaushik12 <71175123+yashavkaushik12@users.noreply.github.com>
2020-09-17 16:46:15 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,48 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getCarsEnd(){
+    database.ref('CarsEnd').on("value", (data)=>{
+      this.rank = data.val();
+    })
+  }
+
+  static updateCarsEnd(rank){
+    database.ref('/').update({
+      CarsEnd: rank
+    });
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
